6add58a4ff2ae5890c4af855dff7c8b98f29d3ac
kunitaya <cocktail.gimlet@gmail.com>
2023-01-19 17:26:43 +0900

    ワークフローで使う変数の作成を追加 - GITHUB_DIRECTORY - IMAGE_NAME - TEMPLATE_NAME

--- jobs/Initialization.sh
+++ jobs/Initialization.sh
@@ -4,6 +4,8 @@
 # GOOGLE_CLOUD_PROJECT_ID: GCP project ID
 # INSTANCE_GROUP_PREFIX: Target instance group prefix
 # NAME: Image source instance name
+# GITHUB_REPOSITORY: Name of Github repository for CI
+# GITHUB_BRANCH: Name of Github branch for CI
 
 # The next line updates PATH for the Google Cloud SDK.
 if [ -f '/usr/local/src/google-cloud-sdk/path.bash.inc' ]; then
@@ -17,6 +19,19 @@ fi
 
 gcloud config set project $GOOGLE_CLOUD_PROJECT_ID
 [ $? -ne 0 ] && exit 1
+
+HASH=`cat /dev/urandom | tr -dc 'a-z0-9' | fold -w 4 | head -1`
+DATE=`date '+%Y%m%d'`
+
+NAME1=`echo $GITHUB_REPOSITORY | sed -e 's/[^a-zA-Z0-9]//g'`
+NAME2=`echo $GITHUB_BRANCH | sed -e 's/[^a-zA-Z0-9]//g'`
+IMAGE_NAME="${NAME1}-${NAME2}-$DATE$HASH"
+TEMPLATE_NAME="${NAME1}-${NAME2}-$DATE$HASH"
+echo "image_name=$IMAGE_NAME" >> $JS7_RETURN_VALUES
+echo "template_name=$TEMPLATE_NAME" >> $JS7_RETURN_VALUES
+
+GITHUB_DIRECTORY=${GITHUB_BRANCH}-$DATE$HASH
+echo "github_directory=$GITHUB_DIRECTORY" >> $JS7_RETURN_VALUES
 
 INSTANCE_GROUPS=`gcloud compute instance-groups list --format='get(name)' --project=$GOOGLE_CLOUD_PROJECT_ID | grep $INSTANCE_GROUP_PREFIX`
 INSTANCE_GROUP_LIST=()
